e998ccfbad1dd2fcb6b3f044f1deca0bf7125c60
amanverma12345 <av7080181848@gmail.com>
2025-04-30 00:05:35 +0530

    foot

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -68,7 +68,7 @@ function Footer() {
           <SocialMediaIcon href={Bio.insta} target="display"><InstagramIcon /></SocialMediaIcon>
         </SocialMediaIcons>
         <Copyright>
-          &copy; 2024 Aman Verma. All rights reserved.
+          &copy; 2025 Aman Verma. All rights reserved.
         </Copyright>
 
       </FooterWrapper>
